47296dc39320f5897024042544831ae04ef57223
rahulsamanta82 <rs4655742@gmail.com>
2025-09-02 17:21:19 +0530

    Your commit message

--- src/utils/helpers/common/axios.config.ts
+++ src/utils/helpers/common/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_PREFIX;
-// axios.defaults.baseURL = 'http://iub.ums.com.pk/api';
+// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_PREFIX;
+axios.defaults.baseURL = 'http://iub.ums.com.pk/api';
 
 export default axios;
